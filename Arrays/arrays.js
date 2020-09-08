d58c000dada9c52f692c89e1b41a8193e5faf74d
Nick Hollins <nholl22@wgu.edu>
2020-09-08 15:51:40 -0400

    understanding arrays

---
+++ Arrays/arrays.js
@@ -0,0 +1,7 @@
+// storing arrays in a variable
+var todos = ['item 1', 'item 2', 'item 3'];
+console.log('My todos:', todos);
+
+// adding another item to todos
+todos.push('item 4');
+console.log(todos);
