d4f3a3713604d12acbb8ba058bbd00196ce62026
Nick Hollins <nholl22@wgu.edu>
2020-09-08 15:56:36 -0400

    array.js updated

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -5,3 +5,13 @@ console.log('My todos:', todos);
 // adding another item to todos
 todos.push('item 4');
 console.log(todos);
+
+// accessing a specific item in array
+console.log(todos[0]);
+console.log(todos[1]);
+console.log(todos[2]);
+console.log(todos[3]);
+
+// changing specific item in array 
+todos[0] = 'item 1 updated';
+console.log(todos[0]);
